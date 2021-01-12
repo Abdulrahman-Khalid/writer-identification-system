5fcce3b216b2edd0117cfb8517131ecb2f7022f7
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-01-12 22:34:56 +0200

    Added some comments

--- image_segmentation.py
+++ image_segmentation.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def image_segmentation(img, threshold):
     # Method One
+    # Apply Dilation to mix all line words or all words characters together
     # kernel = np.ones((3,50), np.uint8)
     # image_dilation = cv2.dilate(np.invert(image), kernel, iterations=1).astype(np.uint8)
     # words_contours,_ = cv2.findContours(image_dilation.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  
