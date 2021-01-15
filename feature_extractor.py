327fa619fcd03e68583b292444a5ee60aedf8b1b
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-15 19:32:35 +0200

    change feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -6,7 +6,7 @@ from skimage.feature import local_binary_pattern
 # no_points = 8 and radius = 3 and method = 'uniform' ----> Accuracy: 85.0%, Average time: 12.17s
 # no_points = 8*3 and radius = 3 and method = 'uniform' ----> accuracy = 90%
 # no_points = 8 and radius = 3 and method = 'default' ----> Accuracy: 100.0%, Average time: 9.73s
-def get_features(gray_lines, binary_lines, radius=3, no_points=8,
+def get_features(gray_lines, binary_lines, radius=3, no_points=4,
                  method='default', verbose=False):
     features = np.zeros(256)
 
@@ -16,9 +16,9 @@ def get_features(gray_lines, binary_lines, radius=3, no_points=8,
         lbp = local_binary_pattern(gray_line, no_points, radius, method=method).astype(np.uint8)
         # if verbose:
         #     print(lbp)
-        features = cv2.calcHist([lbp], [0], binary_line, [256], [0, 256], hist, True).ravel()
+        features = cv2.calcHist([lbp], [0], binary_line, [256], [0, 256], features, True).ravel()
         
-    features /= np.mean(hist)
+    features /= np.mean(features)
 
     return features
 
@@ -70,8 +70,7 @@ def lbp_pixel(gray_image, x, y, radius=3, power_of_2=[1, 2, 4, 8, 16, 32, 64, 12
 
 
 def lbp_features(gray_images, binary_images, radius=3, verbose=False):
-    lbp_features = []
-    hist = np.zeros(256)
+    lbp_features = np.zeros(256)
     power_of_2 = [1, 2, 4, 8, 16, 32, 64, 128]
 
     for idx in range(len(binary_images)):
@@ -80,9 +79,9 @@ def lbp_features(gray_images, binary_images, radius=3, verbose=False):
             for j in range(gray_images[idx].shape[1]):
                 lbp_image[i, j] = lbp_pixel(gray_images[idx], i, j, radius, power_of_2)
         if verbose: print(idx, ":", lbp_image)
-        hist = cv2.calcHist([lbp_image], [0], binary_images[idx], [256], [0, 256], hist, True).ravel()
-    hist /= np.mean(hist)
-    lbp_features.extend(hist)
+        lbp_features = cv2.calcHist([lbp_image], [0], binary_images[idx], [256], [0, 256], lbp_features, True).ravel()
+    lbp_features /= np.mean(lbp_features)
+    lbp_features.extend(lbp_features)
     return lbp_features
 
 
